ac387c53eb19af00ae694f8c661d373091d69ab0
irinashkliarenko <irina.shkliarenko@gmail.com>
2017-06-27 22:10:54 +0200

    gutterGame test added

---
+++ main/bowling/Game.java
@@ -0,0 +1,13 @@
+package bowling;
+
+public class Game {
+
+	public int score() {
+		return 0;
+	}
+
+	public void roll(int pins) {
+		
+	}
+
+}
--- tests/bowling/BowlingGameTests.java
+++ tests/bowling/BowlingGameTests.java
@@ -1,5 +1,22 @@
 package bowling;
 
-public class BowlingGameTests {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+public class BowlingGameTests {
+	
+	Game game = new Game();
+	
+	@Test
+	public void gutterGame() throws Exception {
+		rollMany(20, 0);
+		assertEquals(0, game.score());
+	}
+
+	private void rollMany(int n, int pins) {
+		for(int i=0; i<n; i++) {
+			game.roll(pins);
+		}
+	}
 }
